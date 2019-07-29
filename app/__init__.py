16ffc108e3aa0d9e8647340191d4fe5bd7dc9ef7
melisajuma <Melisaakinyi95@gmail.com>
2019-07-29 13:11:00 +0300

    setting config

--- app/__init__.py
+++ app/__init__.py
@@ -16,11 +16,11 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
 
-    # Registering the blueprint>
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # setting config
+    # setting config>
     from .request import configure_request
     configure_request(app)
 
