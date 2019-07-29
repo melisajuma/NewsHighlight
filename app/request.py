6d4d436d18ea7002b0f98c2dac2735e0d2086d3a
melisajuma <Melisaakinyi95@gmail.com>
2019-07-29 13:16:49 +0300

    Getting api key

--- app/request.py
+++ app/request.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from .models import Source
 from .models import Article
 
-# Getting api key
+# Getting api key>
 api_key = None
 #Getting the movie base url
 base_url = None
